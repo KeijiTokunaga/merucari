92fc8d047984285a2447d701848b99387c88005e
Keiji Tokunaga <keizi81@gmail.com>
2024-06-09 03:36:33 +0000

    アイコン設定と検索窓

--- src/components/Categories.tsx
+++ src/components/Categories.tsx
@@ -1,18 +1,24 @@
-import { categoryMenus } from '../constants';
+import { useState } from "react";
+import { categoryMenus } from "../constants";
 
 export const Categories = () => {
+  const [active, setActive] = useState<number>(0);
   return (
     <div className="border-b border-secondary-border">
       <div className="container mx-auto max-w-screen-xl">
         <div
           className="flex overflow-x-auto px-5 lg:px-10"
-          style={{ scrollbarWidth: 'none' }}
+          style={{ scrollbarWidth: "none" }}
         >
           {categoryMenus.map((menu, index) => (
             <a
+              onClick={() => setActive(index)}
               key={index}
-              href={menu.link}
-              className="relative whitespace-nowrap px-6 py-3 text-sm font-bold text-secondary-text hover:bg-secondary-hover"
+              className={`relative whitespace-nowrap px-6 py-3 text-sm font-bold hover:bg-secondary-hover ${
+                index === active
+                  ? "text-red-500 before:absolute before:bottom-0 before:left-0 before:h-[3px] before:w-full before:bg-red-500"
+                  : "text-secondary-text"
+              }`}
             >
               {menu.label}
             </a>
